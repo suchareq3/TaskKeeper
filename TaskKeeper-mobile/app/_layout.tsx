76dcd36d0f100ac1bcd91533eca34f7e6d596a13
fldmubm <konrad.gruszczynski@eviden.com>
2024-12-01 14:00:51 +0100

    Revert "Added GlueStack to mobile"
    This reverts commit c48811f3baf85a97ecb51749f0c2b38d743aaed3.

--- TaskKeeper-mobile/app/_layout.tsx
+++ TaskKeeper-mobile/app/_layout.tsx
@@ -1,16 +1,13 @@
 import { Slot, Stack } from "expo-router";
 import { SessionProvider } from "@/components/AuthContext";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
   // Set up the auth context and render our layout inside of it.
   return (
-    <GluestackUIProvider>
-      <SessionProvider>
-        <Slot />
-      </SessionProvider>
-    </GluestackUIProvider>
-
+    <SessionProvider>
+      <Slot/>
+    </SessionProvider>
+    
     // <Stack>
     //   <Stack.Screen name="index"/>
     // </Stack>
