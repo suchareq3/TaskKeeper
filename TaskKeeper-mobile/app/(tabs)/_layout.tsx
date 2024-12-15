23c40e86c255b2f8aa8b84ef43f5384cbf322aad
suchareq3 <wierzba41@gmail.com>
2024-12-15 21:06:03 +0100

    Added new tabs

--- TaskKeeper-mobile/app/(tabs)/_layout.tsx
+++ TaskKeeper-mobile/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { useSession } from "@/components/AuthContext";
 import { Redirect, Stack, Tabs } from "expo-router";
 import { Text } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function TabsLayout() {
   const { session, isLoading } = useSession();
@@ -24,10 +25,64 @@ export default function TabsLayout() {
     // using "<Stack/>" here "defers" the layout to either 'index' or whatever's being requested/replaced by the Router
     // however, I do want to use tabs... so I use tabs instead.
     // TODO: perhaps move this to a separate file, for styling and stuff?
-    <Tabs>
-       <Tabs.Screen name="about" />
-       <Tabs.Screen name="index" />
+    <Tabs screenOptions={{ headerShown: false, tabBarStyle: { height: 50, margin: 0, padding: 0, borderColor: "red", borderTopWidth: 0 }, tabBarActiveBackgroundColor: "rgb(200,200,200)" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+          tabBarLabelStyle: { fontSize: 11, fontWeight: "500", paddingBottom: 3 },
+
+          tabBarIcon: ({ color }) => (
+            <AntDesign
+              name="home"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tasks"
+        options={{
+          title: "Tasks",
+          tabBarLabelStyle: { fontSize: 11, fontWeight: "500", paddingBottom: 3 },
+          tabBarIcon: ({ color }) => (
+            <AntDesign
+              name="profile"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="projects"
+        options={{
+          title: "Projects",
+          tabBarLabelStyle: { fontSize: 11, fontWeight: "500", paddingBottom: 3 },
+          tabBarIcon: ({ color }) => (
+            <AntDesign
+              name="solution1"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: "Calendar",
+          tabBarLabelStyle: { fontSize: 11, fontWeight: "500", paddingBottom: 3 },
+          tabBarIcon: ({ color }) => (
+            <AntDesign
+              name="calendar"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
-    
   );
 }
---
+++ TaskKeeper-mobile/app/(tabs)/calendar.tsx
@@ -0,0 +1,33 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function CalendarScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+      <Link
+        href="/"
+        style={styles.goTo}
+      >
+        Go to HOME screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#13569e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  goTo: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
