352e1f09f7f932582f5f54fbefcb1dd6382ab74c
fldmubm <konrad.gruszczynski@eviden.com>
2024-12-24 21:59:52 +0100

    Added a dark/light theme toggle

--- TaskKeeper-mobile/app/(tabs)/_layout.tsx
+++ TaskKeeper-mobile/app/(tabs)/_layout.tsx
@@ -1,13 +1,29 @@
 import { useSession } from "@/components/AuthContext";
 import { Redirect, Stack, Tabs } from "expo-router";
-import { Text, useColorScheme } from "react-native";
+import { Appearance, Button, Text, useColorScheme, } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Drawer from "expo-router/drawer";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { ThemeProvider } from "@react-navigation/native";
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from "@react-navigation/drawer";
+import { DarkTheme, Theme, ThemeProvider, useTheme, } from "@react-navigation/native";
+import { NAV_THEME } from "@/lib/constants";
+import { colorScheme } from "nativewind";
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
 
 export default function TabsLayout() {
   const { session, isLoading } = useSession();
+  const isDarkColorScheme = useColorScheme() === "dark";
+  const colorScheme = Appearance.getColorScheme();
+  console.log("isDarkColorScheme1", colorScheme);
+  const { colors } = useTheme();
+  console.log("colors", colors);
 
   // You can keep the splash screen open, or render a loading screen like we do here.
   if (isLoading) {
@@ -23,23 +39,30 @@ export default function TabsLayout() {
     // in the headless Node process that the pages are rendered in.
     return <Redirect href="/sign-in" />;
   }
+  console.log("isDarkColorScheme2", colorScheme);
 
   const Drawer = createDrawerNavigator();
-  const isDarkColorScheme = useColorScheme();
 
   return (
     // using "<Stack/>" here "defers" the layout to either 'index' or whatever's being requested/replaced by the Router
     // however, I do want to use tabs... so I use tabs instead.
     // TODO: perhaps move this to a separate file, for styling and stuff?
     <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="Tabs"
-          component={TabNavigator}
-          options={{ drawerActiveBackgroundColor: "white", drawerInactiveBackgroundColor: "white", drawerActiveTintColor: "white", headerTintColor: "white", headerPressColor: "white", overlayColor: "white", drawerInactiveTintColor: "white", drawerStyle: { backgroundColor: "white" } }}
-        />
+      <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />} screenOptions={{ headerStatusBarHeight: 0, headerTintColor: colors.text }}>
+        {/* 'screens' for navigation go here! all other content goes in the 'CustomDrawerContent' component!*/}
+        <Drawer.Screen name="Go to Tabs" component={TabNavigator} options={{ headerStatusBarHeight: 0, headerTintColor: colors.text, headerTitle: "TaskKeeper" }} />
       </Drawer.Navigator>
     </ThemeProvider>
+  );
+}
+
+function CustomDrawerContent(props: any) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      {/* <Button title="Sign Out" onPress={() => {console.log("wawawa")}} /> */}
+      <DrawerItem label={"Toggle theme, current: " + colorScheme.get()} onPress={() => { console.log("Theme toggled!", colorScheme.toggle()); }} />
+    </DrawerContentScrollView>
   );
 }
 
--- TaskKeeper-mobile/app/_layout.tsx
+++ TaskKeeper-mobile/app/_layout.tsx
@@ -35,10 +35,6 @@ export default function RootLayout() {
   React.useEffect(() => {
     (async () => {
       const theme = await AsyncStorage.getItem("theme");
-      if (Platform.OS === "web") {
-        // Adds the background color to the html element to prevent white background on overscroll.
-        document.documentElement.classList.add("bg-background");
-      }
       if (!theme) {
         AsyncStorage.setItem("theme", colorScheme);
         setIsColorSchemeLoaded(true);
